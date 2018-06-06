88bbef85905347e3dc17eb9753f6e27c90d722cd
Gustav Andersson <guustav.andersson@gmail.com>
2018-06-06 13:57:39 +0200

    Added so the pun and answer appears in the app

--- app/src/main/java/gustav/ola/puns_riddles/MainActivity.java
+++ app/src/main/java/gustav/ola/puns_riddles/MainActivity.java
@@ -10,8 +10,10 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
     boolean isPun = false;
     boolean isRiddle = false;
-    int counter = 0;                                    //Keep track of which question
-    int temp = 0;                                       //A variable to store counter in
+    int riddleCounter = 0;                                    //Keep track of which question
+    int riddleTemp = 0;                                       //A variable to store counter in
+    int punCounter = 0;
+    int punTemp = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +30,7 @@ public class MainActivity extends Activity {
     public void getPun(View view){
         final TextView punView = findViewById(R.id.question);
         final TextView answerView = findViewById(R.id.answer);
-        punView.setText("Ordvits");
+        punView.setText(setPunQuestion(0));
         answerView.setText("");
         isPun = true;
         isRiddle = false;
@@ -38,7 +40,7 @@ public class MainActivity extends Activity {
     public void getRiddle(View view){
         final TextView riddleView = findViewById(R.id.question);
         final TextView answerView = findViewById(R.id.answer);
-        riddleView.setText(setQuestion(0));                             //Get question
+        riddleView.setText(setRiddleQuestion(0));                             //Get question
         answerView.setText("");
         isPun = false;
         isRiddle = true;
@@ -49,9 +51,9 @@ public class MainActivity extends Activity {
         final TextView answerView = findViewById(R.id.answer);
         if(answerView.getText().equals("")){
             if (isRiddle == true) {
-                answerView.setText(setQuestion(1));
+                answerView.setText(setRiddleQuestion(1));
             }else if(isPun == true){
-                answerView.setText("Svar");
+                answerView.setText(setPunQuestion(1));
             }
         }else{
             if(isRiddle == true && isPun == false){
@@ -62,8 +64,8 @@ public class MainActivity extends Activity {
         }
     }
 
-    //Return question or answer
-    public String setQuestion(int index){           //Index 0 = question 1 = answer
+    //Return question or answer for riddle
+    public String setRiddleQuestion(int index){           //Index 0 = question 1 = answer
 
         int columns = 3;        //Questions
         int rows = 2;           //Answers
@@ -79,15 +81,55 @@ public class MainActivity extends Activity {
         riddles[2][0] = "Om du har den, så vill du dela med dig av den. Men om du delar med dig av den, så har du den inte. Vad är det?";
         riddles[2][1] = "En hemlighet";
 
-        if(counter < columns && index == 0){
-            temp = counter;
-        }else if(counter < columns - index && index == 1){
-            temp = counter;
-            counter++;
+        if(riddleCounter < columns && index == 0){
+            riddleTemp = riddleCounter;
+        }else if(riddleCounter < columns - index && index == 1){
+            riddleTemp = riddleCounter;
+            riddleCounter++;
         }else{
-            counter = 0;
+            riddleCounter = 0;
         }
 
-        return riddles[temp][index];
+        return riddles[riddleTemp][index];
+    }
+
+    public String setPunQuestion(int index){
+        int columns = 7;        //Questions
+        int rows = 2;           //Answers
+
+        String[][] puns = new String[columns][rows];
+
+        puns[0][0] = "Blir du inte trött av att spela schack?";
+        puns[0][1] = "Jo, alldeles matt!";
+
+        puns[1][0] = "Har du en pool hemma?";
+        puns[1][1] = "Nä, men jag har damm under sängen.";
+
+        puns[2][0] = "Vilken snygg fisk du har!";
+        puns[2][1] = "Ja, det är en läcker-ål.";
+
+        puns[3][0] = "Kalle Anka, Bamse och Fantomen har hittats döda.";
+        puns[3][1] = "Polisen tror att en seriemördare går lös.";
+
+        puns[4][0] = "Var det varmt i New York?";
+        puns[4][1] = "Ja, rena Manhettan!";
+
+        puns[5][0] = "Vad vill du ha för svamp?";
+        puns[5][1] = "Hmm... Jag kan ta reller!";
+
+        puns[6][0] = "Är du klar med julgodiset?";
+        puns[6][1] = "Ja, men det tog nästan knäcken på mig!";
+
+
+        if(punCounter < columns && index == 0){
+            punTemp = punCounter;
+        }else if(punCounter < columns - index && index == 1){
+            punTemp = punCounter;
+            punCounter++;
+        }else{
+            punCounter = 0;
+        }
+
+        return puns[punTemp][index];
     }
 }
